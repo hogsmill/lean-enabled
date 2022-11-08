f2c64e7a159ab5c1d4e2ea06ec8cdf70fb4f3436
hogsmill <info@hogsmill.com>
2022-11-08 00:12:04 +0000

    mail

--- src/lib/mail.js
+++ src/lib/mail.js
@@ -7,12 +7,26 @@ const required = {
   email: true
 }
 
+function trim(data) {
+  let ret = ''
+  if (typeof(data) == 'string') {
+    ret = data.trim
+  } else {
+    const arr = []
+    for (let i = 0; i < data.length; i++) {
+      arr.push(data[i].trim())
+    }
+    ret = arr
+  }
+  return ret
+}
+
 const Mail = {
 
   post: function(data, message) {
     let ok = true
     for (const key in data) {
-      data[key] = data[key].trim()
+      data[key] = trim(data[key])
       if (required[key]) {
         if (!data[key] || 0 === data[key].length) {
           ok = false
