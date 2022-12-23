6ce4c7681a2a0266f8f3b061b37f300b3622f19a
hogsmill <info@hogsmill.com>
2022-12-23 08:12:44 +0000

    typo

--- src/store/defaults.js
+++ src/store/defaults.js
@@ -222,7 +222,6 @@ const _defaults = {
       quote: 'Measurement is the first step that leads to control and eventually to improvement. If you can’t measure something, you can’t understand it. If you can’t understand it, you can’t control it. If you can’t control it, you can’t improve it.',
       author: 'H.James Harrington'
     },
-    /*
     {
       quote: 'This is a fundamental truth about any sort of practice: If you never push yourself beyond your comfort zone, you will never improve.',
       author: 'Anders Ericsson'
